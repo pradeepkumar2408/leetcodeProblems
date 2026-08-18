6a84b53d14833bcda8435328c490dcb7150a620e
pradeepkumar2408 <pradeepkamaraj7639@gmail.com>
2026-08-18 09:55:34 +0530

    Time: 5 ms (50.09%), Space: 46.5 MB (98.15%) - LeetHub

---
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.java
@@ -0,0 +1,33 @@
+class Solution {
+    int dfs(boolean[][] vis, int[][] grid, int i, int j, int m, int n) {
+        vis[i][j] = true;
+        int ans = grid[i][j];
+        int[] di = { 1, 0, -1, 0 };
+        int[] dj = { 0, 1, 0, -1 };
+
+        for (int l = 0; l < 4; l++) {
+            int ci = i + di[l], cj = j + dj[l];
+
+            if (ci >= 0 && ci < m && cj >= 0 && cj < n && !vis[ci][cj] && grid[ci][cj] != 0) {
+                ans += dfs(vis, grid, ci, cj, m, n);
+            }
+        }
+        return ans;
+    }
+
+    public int findMaxFish(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+
+        boolean[][] vis = new boolean[m][n];
+        int max = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!vis[i][j] && grid[i][j] != 0) {
+                    int val = dfs(vis, grid, i, j, m, n);
+                    max = Math.max(max, val);
+                }
+            }
+        }
+        return max;
+    }
+}
