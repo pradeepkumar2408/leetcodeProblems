958bb61793436ad9a5c6737da799d10a3ecd559c
pradeepkumar2408 <pradeepkamaraj7639@gmail.com>
2026-08-19 20:22:36 +0530

    Time: 2 ms (75.19%), Space: 47.5 MB (87.99%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -0,0 +1,36 @@
+class Solution {
+    void dfs(boolean[][] vis, char[][] board, int i, int j, int m, int n) {
+        vis[i][j] = true;
+
+        int[] di = { -1, 0, 1, 0 };
+        int[] dj = { 0, 1, 0, -1 };
+
+        for (int l = 0; l < 4; l++) {
+            int ci = i + di[l], cj = j + dj[l];
+
+            if (ci >= 0 && ci < m && cj >= 0 && cj < n && !vis[ci][cj] && board[ci][cj] == 'O')
+                dfs(vis, board, ci, cj, m, n);
+        }
+    }
+
+    public void solve(char[][] board) {
+        int m = board.length, n = board[0].length;
+        boolean[][] vis = new boolean[m][n];
+
+        for(int i = 0; i < m ; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 || i == m - 1 || j == 0 || j == n - 1){
+                    if(!vis[i][j] && board[i][j] == 'O'){
+                        dfs(vis, board, i , j, m , n);
+                    }
+                }
+            }
+        }
+        for(int i = 0; i < m ; i++){
+            for(int j = 0; j < n; j++){
+                if(!vis[i][j] && board[i][j] == 'O')
+                    board[i][j] = 'X';
+            }
+        }
+    }
+}
